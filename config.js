cd12c5258d6257e50a1477f5b9641057715982f2
Sz4by <112946552+Sz4by@users.noreply.github.com>
2024-08-15 11:23:26 +0300

    Update config.js

--- config.js
+++ config.js
@@ -5,13 +5,13 @@ module.exports = {
   commandsDir: './commands',  
   embedColor: "#1db954",
   musicardTheme:"themes16", //Goes from themes1 to themes19
-  activityName: "Szaby", // This is bot status Write Anything here 
+  activityName: "Deadpool & Rozsomák", // This is bot status Write Anything here 
   activityType: "WATCHING",  // Available activity types : LISTENING , PLAYING
   SupportServer: "https://discord.gg/6Mqrsca8Zy",
   CheckmarkIcon: "https://cdn.discordapp.com/attachments/1230824451990622299/1230836684774576168/7762-verified-blue.gif",
   MusicIcon:"https://media.discordapp.net/attachments/1230824451990622299/1236664581364125787/music-play.gif",
   embedTimeout: 5,  // Timeout before the button interaction embeds are deleted ( Default - 5 seconds)
-  errorLog: "", 
+  errorLog: "1158023873057525850", 
 
    // Lavalink Server Details
 
