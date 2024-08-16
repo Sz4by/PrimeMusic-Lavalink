59a80e39c9f21df9818c46c36eaf67e790d764e2
Sz4by <112946552+Sz4by@users.noreply.github.com>
2024-08-16 15:42:08 +0300

    Update config.js

--- config.js
+++ config.js
@@ -18,9 +18,9 @@ module.exports = {
   nodes: [
     {
         name: "Node #1",
-        host: "lavalink.akshtt.is-a.dev",
+        host: "lava-v3.ajieblogs.eu.org",
         port: 443,
-        password: "admin",
+        password: "https://dsc.gg/ajidevserver",
         reconnectTimeout: 5000,
         reconnectTries: Infinity,
         secure: true     
